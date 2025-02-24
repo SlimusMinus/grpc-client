1970a35b6d202e11dde1352d69ff80c7d4c33af0
SlimusMinus <moscow_city231@rambler.ru>
2025-02-24 16:24:18 +0300

    start grpc

--- src/main/java/com/krylov/grpcclient/GrpcClientApplication.java
+++ src/main/java/com/krylov/grpcclient/GrpcClientApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GrpcClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrpcClientApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GrpcClientApplication.class, args);
+  }
 
 }
--- src/main/java/com/krylov/grpcclient/service/GrpcClientService.java
+++ src/main/java/com/krylov/grpcclient/service/GrpcClientService.java
@@ -1,4 +1,25 @@
 package com.krylov.grpcclient.service;
 
+import com.example.grpc.GreetingServiceGrpc.GreetingServiceBlockingStub;
+import com.example.grpc.GreetingServiceOuterClass.HelloRequest;
+import com.example.grpc.GreetingServiceOuterClass.HelloResponse;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GrpcClientService {
+
+@GrpcClient("MyGrpcService")
+  private GreetingServiceBlockingStub greetingServiceBlockingStub;
+
+
+  public String sayHello(String name, String surname){
+    HelloRequest request = HelloRequest.newBuilder()
+        .setName(name)
+        .setSurName(surname)
+        .build();
+
+    HelloResponse response = greetingServiceBlockingStub.greeting(request);
+      return response.getGreeting();
+  }
 }
